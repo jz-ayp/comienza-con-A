e1bad90190aafe2478868f10bedf727f85671f1e
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-27 00:30:30 -0600

    Update main.py
    Delete solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
-
+"""
 
 # Declaraciones
 CONSTANTE = valor
@@ -13,20 +13,3 @@ entrada = input()
 
 # Salidas
 print(salida)
-"""
-"""
-Comienza con A
-"""
-
-# Entradas
-palabra = input("Escriba una palabra: ")
-
-# Proceso
-if len(palabra) > 0 and palabra[0].lower() in "aá":
-    comienza = "comienza"
-else:
-    comienza = "no comienza"
-respuesta = "'" + palabra + "' " + comienza + " con 'A'"
-
-# Salidas
-print(respuesta)
